6ab86cd7a05072844c1d244660bfbf096973cf83
jdunn1998 <dunn2jm@mail.uc.edu>
2025-02-16 17:33:45 -0500

    Finished Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args); {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
 
         //Get user Data
+        System.out.println("Please enter your Age. ");
+        int userAge = in.nextInt();
+
+        //Conditionals
+        if (userAge >= 21){
+            System.out.println("Please collect your wristband! ");}
+        else if (userAge < 21) {
+            System.exit(0);}
+            else {
+                System.out.println("Invalid input. Please enter a valid number.");
+            }
+        }
     }
-}
